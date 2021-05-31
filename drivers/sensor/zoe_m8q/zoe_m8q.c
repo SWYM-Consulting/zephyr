cdbb7c42c2bf645b7e94206a6c5f50ef7eb2e676
Marshall Wingerson <marshall@swym.consulting>
2021-05-31 17:54:32 -0600

    copied files from icm42605 samples, drivers and yaml. Lots of work left to do.

---
+++ drivers/sensor/zoe_m8q/zoe_m8q.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *
+ * @file            zoe_m8q.c
+ *
+ * @brief           Driver for u-blox ZOE-M8Q
+ *
+ * @details         
+ * 
+ * @author(s)       Marshall Wingerson
+ *
+ * @created         05/26/2021   (MM/DD/YYYY)
+ *
+ * @copyright       © 2020 SWYM, LLC. All Rights Reserved.
+ *
+ *****************************************************************************/
+
+#include <drivers/spi.h>
+
---
+++ drivers/sensor/zoe_m8q/zoe_m8q.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *
+ * @file            zoe_m8q.h
+ *
+ * @brief           Driver for u-blox ZOE-M8Q
+ *
+ * @details         Using Zephyr adxl362 as a template
+ * 
+ * @author(s)       Marshall Wingerson
+ *
+ * @created         05/26/2021   (MM/DD/YYYY)
+ *
+ * @copyright       © 2020 SWYM, LLC. All Rights Reserved.
+ *
+ *****************************************************************************/
+
+#ifndef ZOE_M8Q_h_
+#define ZOE_M8Q_h_
+
+
+
+
+
+#endif  // ZOE_M8Q_h_
