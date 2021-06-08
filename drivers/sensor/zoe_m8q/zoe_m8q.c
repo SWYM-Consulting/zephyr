e6e19ce6816ad4d77647d5373d8eba9a93296e47
Marshall Wingerson <marshall@swym.consulting>
2021-06-08 12:52:45 -0600

    James look heregit -C zephyr status! Working stubs of the zoe-m8q driver. No real functionality but the system builds, has infrastructure structs and function stubs

--- drivers/sensor/zoe_m8q/zoe_m8q.c
+++ drivers/sensor/zoe_m8q/zoe_m8q.c
@@ -14,15 +14,65 @@
  *
  *****************************************************************************/
 
+#include <device.h>
+
 #include <drivers/spi.h>
 #include <init.h>
 // #include <sys/byteorder.h>
 #include <drivers/sensor.h>
 #include <logging/log.h>
 
+#include "zoe_m8q.h"
+
 #define DT_DRV_COMPAT u_blox_zoe_m8q
 
 // LOG_MODULE_REGISTER(ZOE-M8Q, CONFIG_SENSOR_LOG_LEVEL);
+
+static struct zoe_m8q_data zoe_m8q_data;
+
+int zoe_m8q_init(const struct device *dev)
+{
+	printk("ZOE_M8Q: init!!!!\n");
+	return 0;
+}
+
+static int zoe_m8q_chip_init(const struct device *dev)
+{
+	printk("ZOE_M8Q: chip init!!!!\n");
+	return 0;
+}
+
+
+
+
+static int zoe_m8q_attr_set(const struct device *dev,
+			    enum sensor_channel chan,
+			    enum sensor_attribute attr,
+			    const struct sensor_value *val)
+{
+	// switch (attr) {
+	// case SENSOR_ATTR_UPPER_THRESH:
+	// case SENSOR_ATTR_LOWER_THRESH:
+	// 	return adxl362_attr_set_thresh(dev, chan, attr, val);
+	// default:
+	// 	/* Do nothing */
+	// 	break;
+	// }
+
+	// switch (chan) {
+	// case SENSOR_CHAN_ACCEL_X:
+	// case SENSOR_CHAN_ACCEL_Y:
+	// case SENSOR_CHAN_ACCEL_Z:
+	// case SENSOR_CHAN_ACCEL_XYZ:
+	// 	return axl362_acc_config(dev, chan, attr, val);
+	// default:
+	// 	LOG_DBG("attr_set() not supported on this channel.");
+	// 	return -ENOTSUP;
+	// }
+
+	return 0;
+}
+
 
 static int zoe_m8q_sample_fetch(const struct device *dev,
 				 enum sensor_channel chan)
@@ -111,3 +161,60 @@ static int zoe_m8q_sample_fetch(const struct device *dev,
 
 	return 0;
 }
+
+
+
+static int zoe_m8q_channel_get(const struct device *dev,
+			       enum sensor_channel chan,
+			       struct sensor_value *val)
+{
+	// struct adxl362_data *data = dev->data;
+
+	// switch (chan) {
+	// case SENSOR_CHAN_ACCEL_X: /* Acceleration on the X axis, in m/s^2. */
+	// 	adxl362_accel_convert(val, data->acc_x, data->selected_range);
+	// 	break;
+	// case SENSOR_CHAN_ACCEL_Y: /* Acceleration on the Y axis, in m/s^2. */
+	// 	adxl362_accel_convert(val, data->acc_y, data->selected_range);
+	// 	break;
+	// case SENSOR_CHAN_ACCEL_Z: /* Acceleration on the Z axis, in m/s^2. */
+	// 	adxl362_accel_convert(val, data->acc_z,  data->selected_range);
+	// 	break;
+	// case SENSOR_CHAN_DIE_TEMP: /* Temperature in degrees Celsius. */
+	// 	adxl362_temp_convert(val, data->temp);
+	// 	break;
+	// default:
+	// 	return -ENOTSUP;
+	// }
+
+	return 0;
+}
+
+static const struct sensor_driver_api zoe_m8q_api_funcs = {
+	.attr_set     = zoe_m8q_attr_set,
+	.sample_fetch = zoe_m8q_sample_fetch,
+	.channel_get  = zoe_m8q_channel_get,
+// #ifdef CONFIG_ZOE_M8Q_TRIGGER
+// 	.trigger_set = zoe_m8q_trigger_set,
+// #endif
+};
+
+static const struct zoe_m8q_config zoe_m8q_config = {
+	.spi_name = DT_INST_BUS_LABEL(0),
+	.spi_slave = DT_INST_REG_ADDR(0),
+// 	.spi_max_frequency = DT_INST_PROP(0, spi_max_frequency),
+// #if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+// 	.gpio_cs_port = DT_INST_SPI_DEV_CS_GPIOS_LABEL(0),
+// 	.cs_gpio = DT_INST_SPI_DEV_CS_GPIOS_PIN(0),
+// 	.cs_flags = DT_INST_SPI_DEV_CS_GPIOS_FLAGS(0),
+// #endif
+// #if defined(CONFIG_ADXL362_TRIGGER)
+// 	.gpio_port = DT_INST_GPIO_LABEL(0, int1_gpios),
+// 	.int_gpio = DT_INST_GPIO_PIN(0, int1_gpios),
+// 	.int_flags = DT_INST_GPIO_FLAGS(0, int1_gpios),
+// #endif
+};
+
+DEVICE_DT_INST_DEFINE(0, zoe_m8q_init, device_pm_control_nop,
+		    &zoe_m8q_data, &zoe_m8q_config, POST_KERNEL,
+		    CONFIG_SENSOR_INIT_PRIORITY, &zoe_m8q_api_funcs);
--- drivers/sensor/zoe_m8q/zoe_m8q.h
+++ drivers/sensor/zoe_m8q/zoe_m8q.h
@@ -19,6 +19,57 @@
 
 
 
+struct zoe_m8q_config {
+	char *spi_name;
+	uint32_t spi_max_frequency;
+	uint16_t spi_slave;
+// #if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+// 	const char *gpio_cs_port;
+// 	gpio_pin_t cs_gpio;
+// 	gpio_dt_flags_t cs_flags;
+// #endif
+// #if defined(CONFIG_ADXL362_TRIGGER)
+// 	const char *gpio_port;
+// 	gpio_pin_t int_gpio;
+// 	gpio_dt_flags_t int_flags;
+// 	uint8_t int1_config;
+// 	uint8_t int2_config;
+// #endif
+};
+
+struct zoe_m8q_data {
+	const struct device *spi;
+	struct spi_config spi_cfg;
+// #if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+// 	struct spi_cs_control adxl362_cs_ctrl;
+// #endif
+	// int16_t acc_x;
+	// int16_t acc_y;
+	// int16_t acc_z;
+	// int16_t temp;
+	// uint8_t selected_range;
+
+// #if defined(CONFIG_ADXL362_TRIGGER)
+// 	const struct device *dev;
+// 	const struct device *gpio;
+// 	struct gpio_callback gpio_cb;
+// 	struct k_mutex trigger_mutex;
+
+// 	sensor_trigger_handler_t th_handler;
+// 	struct sensor_trigger th_trigger;
+// 	sensor_trigger_handler_t drdy_handler;
+// 	struct sensor_trigger drdy_trigger;
+
+// #if defined(CONFIG_ADXL362_TRIGGER_OWN_THREAD)
+// 	K_KERNEL_STACK_MEMBER(thread_stack, CONFIG_ADXL362_THREAD_STACK_SIZE);
+// 	struct k_sem gpio_sem;
+// 	struct k_thread thread;
+// #elif defined(CONFIG_ADXL362_TRIGGER_GLOBAL_THREAD)
+// 	struct k_work work;
+// #endif
+// #endif /* CONFIG_ADXL362_TRIGGER */
+};
+
 // static int zoe_m8q_sample_fetch(const struct device *dev,
 // 				 enum sensor_channel chan);
 
